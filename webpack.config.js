8d4164aa13051f8f4b4fb83d7c8a55452e2c06a7
ljaviertovar <luisjavier_tovar@outlook.com>
2021-03-16 09:23:22 -0600

    [ADD] fonts loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: 'main.js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     resolve: {
         extensions: ['.js']
@@ -28,9 +29,23 @@ module.exports = {
                     "stylus-loader"]
             },
             {
-                test: /\.png/,
-                type: 'assets/resource'
+                test: /\.png$/,
+                type: 'asset/resource'
             },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: "application/font-woff",
+                        name: "[name].[ext]",
+                        outputPath: "./assets/fonts",
+                        publicPath: "./assets/fonts",
+                        esModule: false
+                    }
+                }
+            }
         ]
     },
     plugins: [
